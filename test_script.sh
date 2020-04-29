c53d02b1104890b10c5f2679455789da056f09c1
Matt Packwood <mattpackwood@gmail.com>
2020-04-29 18:40:29 -0400

    Getting a test case working

--- test_script.sh
+++ test_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #shell script to trigger an AppleScript call
-
-osascript test_applescript.applescript
+touch /Users/mattpackwood/Documents/GitHub/TRIGGERcmd-testing/test.txt
+osascript /Users/mattpackwood/Documents/GitHub/TRIGGERcmd-testing/test_applescript.applescript
