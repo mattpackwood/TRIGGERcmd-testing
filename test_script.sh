161c9dd9b65ac4f8d64d63bc87396e04c08e7895
Matt Packwood <mattpackwood@gmail.com>
2020-05-03 13:36:55 -0400

    Added Comments

--- test_script.sh
+++ test_script.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-#shell script to trigger an AppleScript call
+# Basic shell script to trigger an AppleScript call
+
+# First create a file to see if the script was executed 
 touch /Users/mattpackwood/Documents/GitHub/TRIGGERcmd-testing/test.txt
+
+# Next trigger an AppleScript action to speak a confirmation
 osascript /Users/mattpackwood/Documents/GitHub/TRIGGERcmd-testing/test_applescript.applescript
